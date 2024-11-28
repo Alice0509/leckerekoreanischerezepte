b149fe926a6cc6e68ba0a6098a4e3dabcc008faa
“Alice0509 <soojin527@gamil.com>
2024-11-28 23:28:20 +0100

    Add next-sitemap configuration

---
+++ next-sitemap.config.js
@@ -0,0 +1,16 @@
+// next-sitemap.config.js
+
+module.exports = {
+  siteUrl: 'https://www.leckere-koreanische-rezepte.de',
+  generateRobotsTxt: true, // robots.txt 파일 생성 여부
+  // 추가 설정 (선택 사항)
+  // exclude: ['/exclude-page', '/another-page'], // 사이트맵에서 제외할 경로
+  // robotsTxtOptions: {
+  //   policies: [
+  //     {
+  //       userAgent: '*',
+  //       allow: '/',
+  //     },
+  //   ],
+  // },
+};
