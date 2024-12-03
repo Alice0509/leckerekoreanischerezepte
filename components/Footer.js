834e6f9ad16718987d2c85a15acb6068d60e7ea3
“Alice0509 <soojin527@gamil.com>
2024-12-03 07:53:19 +0100

    Fix Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,7 @@
 // components/Footer.js
-import React from 'react';
-import styles from '../styles/Footer.module.css';
-import { useRouter } from 'next/router';
 import { FaInstagram } from 'react-icons/fa';
+import { useRouter } from 'next/router';
+import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
   const router = useRouter();
@@ -11,22 +10,31 @@ const Footer = () => {
 
   return (
     <footer className={styles.footer}>
-      <p>
-        &copy; {new Date().getFullYear()}{' '}
-        {mappedLocale === 'de'
-          ? 'Alle Rechte vorbehalten.'
-          : 'All rights reserved.'}
-      </p>
-      <p>{mappedLocale === 'de' ? 'Erstellt von Joan.' : 'Created by Joan.'}</p>
-      {/* 인스타그램 링크 */}
-      <a
-        href="https://www.instagram.com/germanhansik"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.instagramLink}
-      >
-        <FaInstagram size={24} />
-      </a>
+      <div className={styles.footerContent}>
+        {/* 저작권 텍스트 */}
+        <p>
+          &copy; {new Date().getFullYear()}{' '}
+          {mappedLocale === 'de'
+            ? 'Alle Rechte vorbehalten.'
+            : 'All rights reserved.'}
+        </p>
+
+        {/* 작성자 텍스트 */}
+        <p>
+          {mappedLocale === 'de' ? 'Erstellt von Joan.' : 'Created by Joan.'}
+        </p>
+
+        {/* 인스타그램 버튼 */}
+        <a
+          href="https://www.instagram.com/germanhansik"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.instagramLink}
+          aria-label="Visit our Instagram page"
+        >
+          <FaInstagram size={24} />
+        </a>
+      </div>
     </footer>
   );
 };
