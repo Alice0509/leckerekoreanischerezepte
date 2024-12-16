07b53f38ba4cc7b23c6933cf517743b8c5ad3b0b
“Alice0509 <soojin527@gamil.com>
2024-12-16 14:06:14 +0100

    Fix

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -7,7 +7,7 @@ const PrivacyPolicy = () => (
   <>
     <Head>
       <title>
-        Privacy Policy - Leckere Koreanische Rezepte & HansikYoungRecipes
+        Privacy Policy - Leckere Koreanische Rezepte &amp; HansikYoungRecipes
       </title>
       <meta
         name="description"
@@ -63,8 +63,8 @@ const PrivacyPolicy = () => (
           <p>
             <strong>Affiliate</strong> means an entity that controls, is
             controlled by or is under common control with a party, where
-            "control" means ownership of 50% or more of the shares, equity
-            interest or other securities entitled to vote for election of
+            &quot;control&quot; means ownership of 50% or more of the shares,
+            equity interest or other securities entitled to vote for election of
             directors or other managing authority.
           </p>
         </li>
@@ -76,9 +76,10 @@ const PrivacyPolicy = () => (
         </li>
         <li>
           <p>
-            <strong>Company</strong> (referred to as either "the Company", "We",
-            "Us" or "Our" in this Agreement) refers to Leckere Koreanische
-            Rezepte & HansikYoungRecipes.
+            <strong>Company</strong> (referred to as either &quot;the
+            Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in
+            this Agreement) refers to Leckere Koreanische Rezepte &amp;
+            HansikYoungRecipes.
           </p>
         </li>
         <li>
@@ -161,7 +162,7 @@ const PrivacyPolicy = () => (
         Usage Data is collected automatically when using the Service.
       </p>
       <p className={styles.paragraph}>
-        Usage Data may include information such as Your Device's Internet
+        Usage Data may include information such as Your Device&apos;s Internet
         Protocol address (e.g. IP address), browser type, browser version, the
         pages of our Service that You visit, the time and date of Your visit,
         the time spent on those pages, unique device identifiers and other
@@ -198,14 +199,14 @@ const PrivacyPolicy = () => (
       <p className={styles.paragraph}>
         If You decide to register through or otherwise grant us access to a
         Third-Party Social Media Service, We may collect Personal data that is
-        already associated with Your Third-Party Social Media Service's account,
-        such as Your name, Your email address, Your activities or Your contact
-        list associated with that account.
+        already associated with Your Third-Party Social Media Service&apos;s
+        account, such as Your name, Your email address, Your activities or Your
+        contact list associated with that account.
       </p>
       <p className={styles.paragraph}>
         You may also have the option of sharing additional information with the
-        Company through Your Third-Party Social Media Service's account. If You
-        choose to provide such information and Personal Data, during
+        Company through Your Third-Party Social Media Service&apos;s account. If
+        You choose to provide such information and Personal Data, during
         registration or otherwise, You are giving the Company permission to use,
         share, and store it in a manner consistent with this Privacy Policy.
       </p>
@@ -223,8 +224,8 @@ const PrivacyPolicy = () => (
       <p className={styles.paragraph}>
         We use this information to provide features of Our Service, to improve
         and customize Our Service. The information may be uploaded to the
-        Company's servers and/or a Service Provider's server or it may be simply
-        stored on Your device.
+        Company&apos;s servers and/or a Service Provider&apos;s server or it may
+        be simply stored on Your device.
       </p>
       <p className={styles.paragraph}>
         You can enable or disable access to this information at any time,
@@ -262,10 +263,11 @@ const PrivacyPolicy = () => (
           <p>
             <strong>To contact You:</strong> To contact You by email, telephone
             calls, SMS, or other equivalent forms of electronic communication,
-            such as a mobile application's push notifications regarding updates
-            or informative communications related to the functionalities,
-            products or contracted services, including the security updates,
-            when necessary or reasonable for their implementation.
+            such as a mobile application&apos;s push notifications regarding
+            updates or informative communications related to the
+            functionalities, products or contracted services, including the
+            security updates, when necessary or reasonable for their
+            implementation.
           </p>
         </li>
         <li>
@@ -367,12 +369,13 @@ const PrivacyPolicy = () => (
 
       <h3 className={styles.heading3}>Transfer of Your Personal Data</h3>
       <p className={styles.paragraph}>
-        Your information, including Personal Data, is processed at the Company's
-        operating offices and in any other places where the parties involved in
-        the processing are located. It means that this information may be
-        transferred to — and maintained on — computers located outside of Your
-        state, province, country or other governmental jurisdiction where the
-        data protection laws may differ than those from Your jurisdiction.
+        Your information, including Personal Data, is processed at the
+        Company&apos;s operating offices and in any other places where the
+        parties involved in the processing are located. It means that this
+        information may be transferred to — and maintained on — computers
+        located outside of Your state, province, country or other governmental
+        jurisdiction where the data protection laws may differ than those from
+        Your jurisdiction.
       </p>
       <p className={styles.paragraph}>
         Your consent to this Privacy Policy followed by Your submission of such
@@ -450,7 +453,7 @@ const PrivacyPolicy = () => (
         security.
       </p>
 
-      <h2 className={styles.heading2}>Children's Privacy</h2>
+      <h2 className={styles.heading2}>Children&apos;s Privacy</h2>
       <p className={styles.paragraph}>
         Our Service does not address anyone under the age of 13. We do not
         knowingly collect personally identifiable information from anyone under
@@ -463,7 +466,7 @@ const PrivacyPolicy = () => (
       <p className={styles.paragraph}>
         If We need to rely on consent as a legal basis for processing Your
         information and Your country requires consent from a parent, We may
-        require Your parent's consent before We collect and use that
+        require Your parent&apos;s consent before We collect and use that
         information.
       </p>
 
@@ -471,8 +474,8 @@ const PrivacyPolicy = () => (
       <p className={styles.paragraph}>
         Our Service may contain links to other websites that are not operated by
         Us. If You click on a third party link, You will be directed to that
-        third party's site. We strongly advise You to review the Privacy Policy
-        of every site You visit.
+        third party&apos;s site. We strongly advise You to review the Privacy
+        Policy of every site You visit.
       </p>
       <p className={styles.paragraph}>
         We have no control over and assume no responsibility for the content,
@@ -486,8 +489,8 @@ const PrivacyPolicy = () => (
       </p>
       <p className={styles.paragraph}>
         We will let You know via email and/or a prominent notice on Our Service,
-        prior to the change becoming effective and update the "Last updated"
-        date at the top of this Privacy Policy.
+        prior to the change becoming effective and update the &quot;Last
+        updated&quot; date at the top of this Privacy Policy.
       </p>
       <p className={styles.paragraph}>
         You are advised to review this Privacy Policy periodically for any
