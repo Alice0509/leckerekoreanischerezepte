27a6bedd193ec20fc6c67f3a0116e29e0c57553b
“Alice0509 <soojin527@gamil.com>
2026-07-14 20:45:07 +0200

    Exclude unstable ingredient detail URLs from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,34 @@
 // next-sitemap.config.js
 
 module.exports = {
-  siteUrl: 'https://www.leckere-koreanische-rezepte.de',
-  generateRobotsTxt: true, // robots.txt 파일 생성 여부
-  // 추가 설정 (선택 사항)
-  // exclude: ['/exclude-page', '/another-page'], // 사이트맵에서 제외할 경로
-  // robotsTxtOptions: {
-  //   policies: [
-  //     {
-  //       userAgent: '*',
-  //       allow: '/',
-  //     },
-  //   ],
-  // },
+  siteUrl: 'https://www.hansikyoung.com',
+  generateRobotsTxt: true,
+  autoLastmod: true,
+  exclude: [
+    '/de',
+    '/de/*',
+    '/en',
+    '/en/*',
+    '/page/*',
+    '/datenschutzerklaerung',
+    '/privacy-policy',
+    '/impressum',
+    '/imprint',
+    '/delete-data',
+    '/ingredients/*',
+  ],
+  alternateRefs: [
+    {
+      href: 'https://www.hansikyoung.com',
+      hreflang: 'en',
+    },
+    {
+      href: 'https://www.leckere-koreanische-rezepte.de',
+      hreflang: 'de',
+    },
+    {
+      href: 'https://www.hansikyoung.com',
+      hreflang: 'x-default',
+    },
+  ],
 };
