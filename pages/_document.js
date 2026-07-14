ed1e1749a12660c797a2dbde17a71fd766b83977
“Alice0509 <soojin527@gamil.com>
2026-07-14 20:48:19 +0200

    Let Next.js set html language by locale

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="de">
+      <Html>
         <Head>
           {/* Inter Font Import */}
           <link
